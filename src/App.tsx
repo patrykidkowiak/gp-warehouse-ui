06f5de9f956f61c8ad38758904ada0d9d5ee2ccb
idkowiakp <pidkowiak@tier2consulting.com>
2022-03-22 19:40:14 +0100

    test logs added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Warehouse } from './component/Warehouse';
 
 function App() {
+    console.log(`NODE_ENV:  ${process.env.NODE_ENV}`);
   return (
     <div className="App">
       <Warehouse/>
