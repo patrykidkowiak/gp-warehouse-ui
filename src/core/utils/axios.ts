c5cb9ffed9b28f35f19f6cf28c0db9355e15b07f
idkowiakp <pidkowiak@tier2consulting.com>
2022-03-22 19:20:19 +0100

    axios prod config changed

--- src/core/utils/axios.ts
+++ src/core/utils/axios.ts
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-export const GP_WAREHOUSE_BACKEND = process.env.NODE_ENV === 'production' ? 'https://gp-warehouse.herokuapp.com/' : 'http://localhost:8080';
+export const GP_WAREHOUSE_BACKEND = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://gp-warehouse.herokuapp.com/';
 
 const axios = Axios.create({
     baseURL: GP_WAREHOUSE_BACKEND,
