0f9369c284ddcc78a56d4f89ce3697e8fd8c4ec3
idkowiakp <pidkowiak@tier2consulting.com>
2022-03-22 19:56:15 +0100

    try with hardcoded backend url

--- src/core/utils/axios.ts
+++ src/core/utils/axios.ts
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
-export const GP_WAREHOUSE_BACKEND = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://gp-warehouse.herokuapp.com/';
+// export const GP_WAREHOUSE_BACKEND = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://gp-warehouse.herokuapp.com/';
+export const GP_WAREHOUSE_BACKEND = 'https://gp-warehouse.herokuapp.com/';
 
 const axios = Axios.create({
     baseURL: GP_WAREHOUSE_BACKEND,
